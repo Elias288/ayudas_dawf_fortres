7c9b66b7ef8e1c20d6ed13872644a6e795589af4
Elias288 <bianchi.elias@gmail.com>
2024-03-21 17:20:30 -0300

    fix: Hash router

--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import SideNavProvider from "./providers/navProvider.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SideNavProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </SideNavProvider>
   </React.StrictMode>
 );
